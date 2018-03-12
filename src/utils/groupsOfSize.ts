e687784ee94c327ec6f7f69da407f74cc2e40f6f
peterwmwong <peter.wm.wong@gmail.com>
2018-03-11 19:59:28 -0500

    Add some docs to groupOfSize (testing VSCode intellisense)

--- src/utils/groupsOfSize.ts
+++ src/utils/groupsOfSize.ts
@@ -1,5 +1,22 @@
-export default <T>(input:T[], size:number):T[][] => {
-  const arr = new Array(input.length / size);
+/**
+ * Creates an array of arrays.  Each array has a length of `size`
+ *
+ * Example:
+ *
+ * ```ts
+ *  groupOfSize([1, 2, 3, 4], 2);
+ *  // [[1, 2], [3, 4]]
+ *
+ *  groupOfSize([1, 2, 3], 2);
+ *  // [[1, 2], [3]]
+ * ```
+ *
+ * @param input An array that will be grouped
+ * @param size  Size of each group
+ */
+const groupOfSize = <T>(input:T[], size:number):T[][] => {
+  const arr = new Array((input.length / size) | 0);
   for(let i = 0; i < arr.length;) arr[i] = input.slice(size * i, size * ++i);
   return arr;
 }
+export default groupOfSize;
